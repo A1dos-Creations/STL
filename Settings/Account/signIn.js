28e281921f82cfbbcc78c42116716db81b71ca40
A1dos-Creations <unrelatablexfilms@gmail.com>
2025-02-01 12:58:00 -0800

    Upade 2 for sign-in

--- Settings/Account/signIn.js
+++ Settings/Account/signIn.js
@@ -1,3 +1,3 @@
 document.getElementById('googleSignIn').addEventListener('click', () => {
-    window.open('https://yourdomain.com/auth.html', '_blank', 'width=500,height=600'); // Opens the auth.html from A1dos Creations home website in a new window to sign in with Google.
+    window.open('https://a1dos-creations.com/auth.html', '_blank', 'width=500,height=600'); // Opens the auth.html from A1dos Creations home website in a new window to sign in with Google.
   });
